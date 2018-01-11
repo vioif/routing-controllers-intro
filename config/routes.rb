a4555d126fbcd5d84a3d3a6f8b6e4fc2b3c51b41
vioif <ireneaflores3@gmail.com>
2018-01-10 22:26:00 -0500

    made final changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/welcome' => 'pages#welcome'
+  root 'pages#welcome'
   get '/about'   => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size'  => 'pages#kitten'
+  get '/kitten/:size'  => 'pages#kitten', as: 'kitten'
   get '/secrets/:magic_word' => 'pages#secrets'
 end
